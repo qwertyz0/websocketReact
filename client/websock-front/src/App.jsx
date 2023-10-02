4b231b6494b6ca1d19f3336e555e0c4528458d3c
YanisPC <ynis1998@gmail.com>
2023-10-02 21:04:58 +0300

    connecting to server from client

--- client/websock-front/src/App.jsx
+++ client/websock-front/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+//import and connect socket lib for client
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:3001/");
 
 function App() {
+  const sendMessage = () => {};
+
+  socket.on("hello", (serverSide) => {
+    console.log(`${serverSide}`);
+  });
+
   return (
-    <div>Websocket app</div>
-  )
+    <div className="box">
+      <div className="title">Websocket app</div>
+      <input className="form" type="text" placeholder="Message here" />
+      <button className="button-send">Send message</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,33 @@
-console.log('server is started demon');
+/** socket.io is like full stack project that need 'cors' for communicate with frontend 
+Also use express and http lib for creating server
+*/
+const PORT = 3001;
+//instance import
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+//creating new server with http
+const server = http.createServer(app);
+
+//wrap created server in socket.io lib for connecting with frontend, and use cors for avoid conflicts
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`user ${socket.id} connected to server`);
+
+  socket.emit("hello", "hello Sashenka");
+});
+
+server.listen(PORT, () => {
+  console.log(`server starting on port ${PORT} ...`);
+});
